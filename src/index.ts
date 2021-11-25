7956037b9ab1be5885aa571ff8087d371e48fde4
ice breaker <1324318532@qq.com>
2021-11-25 14:55:35 +0800

    feat: project init and release 1.0.0

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,91 @@
-export const foo = 'bar'
+type AnyObject = Record<string, any>
 
-export const wait = (timeout?: number) => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(true)
-    }, timeout)
+type UnionProp = string | symbol | number
+
+export interface IBaseContext{
+  invokePath: UnionProp[]
+  [Key:string]: any
+}
+
+function createContext ():IBaseContext {
+  return {
+    invokePath: []
+  }
+}
+
+export interface IDefaultResponse{
+  errCode:number
+  errMsg:string
+  invokePath:string
+  target:any
+  args:any[]
+}
+
+function getDefaultResponse (path:string, target:any, args:any[]):IDefaultResponse {
+  return {
+    errCode: 0,
+    errMsg: 'Mock Api',
+    invokePath: path,
+    target,
+    args
+  }
+}
+
+export interface ICreateProxyOptions{
+  /**
+   * @default ()=>
+   * errCode: 0,
+   * errMsg: 'Mock Api',
+   * invokePath: path,
+   * target,
+   * args
+   */
+  handler?:(path:string, target:any, getDefaultRes:()=>IDefaultResponse)=> any
+}
+
+export function createProxy<T extends AnyObject> (obj: AnyObject = {}, options?:ICreateProxyOptions) {
+  const { handler } = options ?? {}
+
+  function createFuncByTarget<T> (target:T, ctx:IBaseContext) {
+    return function (...args:any[]) {
+      const path = ctx.invokePath.join('.')
+      if (handler) {
+        if (typeof handler === 'function') {
+          return handler(path, target, () => {
+            return getDefaultResponse(path, target, args)
+          })
+        } else {
+          throw new TypeError('handler must be a function !')
+        }
+      }
+      return getDefaultResponse(path, target, args)
+    }
+  }
+
+  function createGetHandler () {
+    return function handler (target:T, prop:UnionProp, receiver:any, ctx:IBaseContext) {
+      ctx.invokePath.push(prop)
+      const tmp = target ? target[prop as string] ?? {} : target
+      const wrapFunc = createFuncByTarget<T>(tmp, ctx)
+      return getProxy(wrapFunc as unknown as T, prop, ctx)
+    }
+  }
+
+  const get = createGetHandler()
+
+  function getProxy (parent: T, prop: UnionProp, ctx:IBaseContext): T {
+    const proxy = new Proxy(parent, {
+      get (target:T, prop:UnionProp, receiver:any) {
+        return get(target, prop, receiver, ctx)
+      }
+    })
+    return proxy
+  }
+
+  return new Proxy(obj, {
+    get (target:T, prop:UnionProp, receiver:any) {
+      const ctx = createContext()
+      return get(target, prop, receiver, ctx)
+    }
   })
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,12 +1,58 @@
-import { foo, wait } from '../src'
+import { createProxy } from '@/index'
 
-describe('[Default]', () => {
-  test('foo should be bar', () => {
-    expect(foo).toBe('bar')
+let obj :Record<string, any>
+let mockObj :Record<string, any>
+beforeAll(() => {
+  obj = createProxy({})
+  mockObj = createProxy({}, {
+    handler (path, t, done) {
+      switch (path) {
+        case 'a.1.b':{
+          return new Promise((resolve, reject) => {
+            resolve({
+              a: 'b',
+              t
+            })
+          })
+        }
+        case 'b.10.a':{
+          return {
+            b: 'a',
+            t
+          }
+        }
+      }
+      return done()
+    }
+  })
+})
+describe('[Mock](proxy) test group', () => {
+  test('default test openapi', async () => {
+    const params = {
+      img: []
+    }
+    const res = await obj.aaa(params)
+    expect(res.errCode).toBe(0)
+    expect(res.invokePath).toBe('aaa')
+    expect(res.args[0]).toEqual(params)
+    const res1 = await obj.search.imageSearch(params)
+    expect(res1.errCode).toBe(0)
+    expect(res1.invokePath).toBe('search.imageSearch')
+    expect(res1.args[0]).toEqual(params)
+    const res2 = await obj.search.aaa.imageSearch(params)
+    expect(res2.errCode).toBe(0)
+    expect(res2.invokePath).toBe('search.aaa.imageSearch')
+    expect(res2.args[0]).toEqual(params)
+    const res3 = await obj.a[1]['12'](params)
+    expect(res3.invokePath).toBe('a.1.12')
   })
 
-  test('wait 100ms', async () => {
-    const flag = await wait(100)
-    expect(flag).toBe(true)
+  test('default test option mapper', async () => {
+    const res = mockObj.a.a.a.a()
+    expect(res.invokePath).toBe('a.a.a.a')
+    const res1 = await mockObj.a[1].b()
+    expect(res1.a).toBe('b')
+    const res2 = mockObj.b[10].a()
+    expect(res2.b).toBe('a')
   })
 })
